0167f45834c32ffd7f5fb3e3875ad576cb887d6c
Nagateja <nagateja.cbit@gmail.com>
2026-07-21 01:23:59 +0530

    Initial ss

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // Configuration for GATE EC 2027 Tracker
 const CONFIG = {
     // Replace this with your deployed Google Apps Script Web App URL
-    SCRIPT_URL: 'https://docs.google.com/spreadsheets/d/1tGTme4Yx1AwgZ8DcUj2aBFnqlvM4iwn3_19aLdJf-lA/edit',
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzTFlhcslKHU8EnnjPdAegSlXS4oa8vQ6jGt0Ueh3zTJ5A27eEweqXk-YQ77FrwRV18wQ/exec',
     
     // The shared secret you chose during setup
     TOKEN: 'GATE2027-xK9mP2qR-NAGA'
