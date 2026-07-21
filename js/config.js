cc245929b08cd2cd8287b50d23fa89e6cbf0798b
Nagateja <141476998+nagatejaa@users.noreply.github.com>
2026-07-21 14:48:40 +0530

    Update SCRIPT_URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,10 @@
 // Configuration for GATE EC 2027 Tracker
+// Configuration for GATE EC 2027 Tracker
 const CONFIG = {
     // Replace this with your deployed Google Apps Script Web App URL
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzTFlhcslKHU8EnnjPdAegSlXS4oa8vQ6jGt0Ueh3zTJ5A27eEweqXk-YQ77FrwRV18wQ/exec',
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbx88HPCM5CfEZctFcR250t9ax73pVdmdc0ONOKRpP4RTbpYOSLqiJA14PVxNGHhNWkItg/exec',
     
     // The shared secret you chose during setup
     TOKEN: 'GATE2027-xK9mP2qR-NAGA'
 };
+
